75d6c8cc7d14bb9bc78547dc81fe02bc444e9ef9
github username <github email address>
2017-06-07 17:47:56 -0400

    final build

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -7,6 +7,19 @@ def get_character_movies_from_api(character)
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
 
+  wanted_person_hash = find_person(character,character_hash)
+
+  while wanted_person_hash == nil
+    break if character_hash["next"] == nil
+    all_characters = RestClient.get(character_hash["next"])
+    character_hash = JSON.parse(all_characters)
+    wanted_person_hash = find_person(character,character_hash)
+  end
+  #hash of "luke"
+  # binding.pry
+  our_films = find_films(wanted_person_hash)
+# binding.pry
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,15 +29,54 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  # Find the person (Luke Skywalker)
+  # Find films
 end
 
-def parse_character_movies(films_hash)
+
+def shorten_results(character_hash)
+  # removes everything from the character_hash except the "results" key.
+  result = character_hash["results"]
+  # array of results
+end
+
+def find_person(sw_char, character_hash)
+  # takes in the original hash, calls shorten_results on it, and returns the requested person as a hash (not an array.)
+   result = shorten_results(character_hash).select do |person, trash|
+    #  binding.pry
+    person["name"].downcase == sw_char
+  end[0]
+
+  #  binding.pry
+end
+ #
+ # character_hash = get_character_movies_from_api("Darth Vader")
+#
+  # puts find_person("Darth Vader", character_hash)
+
+def find_films(single_character_hash)
+  # returns an array of film Urls for the given character_hash.
+  # binding.pry
+  single_character_hash["films"]
+end
+
+# find_films("Darth Vader", character_hash)
+
+def parse_character_movies(films_array)
   # some iteration magic and puts out the movies in a nice list
+  # {"results" => [].select("films")}
+  films_array.map do |film_url|
+    JSON.parse(RestClient.get(film_url))["title"]
+  end
+
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  films_array = get_character_movies_from_api(character)
+  parse_character_movies(films_array).each do |movie|
+    puts movie
+  end
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,12 +1,12 @@
 def welcome
   # puts out a welcome message here!
   puts "Welcome to the Star Wars API Project"
-  puts "test"
+
 end
 
 def get_character_from_user
   puts "please enter a character"
-  result = gets.chomp
+
   # use gets to capture the user's input. This method should return that input, downcased.
   user_input = gets.chomp
   user_input.downcase
